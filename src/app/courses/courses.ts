970fdf3c096dab40593e15eba94b040e8cc7b980
Charles Brendon <charlesbrendonss@gmail.com>
2026-01-03 11:53:02 -0300

    feat: visual da tabela com mat-card concluído

--- src/app/courses/courses.ts
+++ src/app/courses/courses.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { Course } from './model/course';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card'; // Adicione este
+import { MatToolbarModule } from '@angular/material/toolbar'; // Adicione este
+
 @Component({
   selector: 'app-courses',
-  imports: [MatTableModule],
+  standalone: true,
+  imports: [MatTableModule, MatCardModule, MatToolbarModule], // Adicione aqui também
   templateUrl: './courses.html',
   styleUrl: './courses.scss'
 })
 export class CoursesComponent {
+  // Verifique se estas propriedades estão EXATAMENTE assim:
   courses: Course[] = [
     { _id: '1', name: 'Angular', category: 'front-end' }
   ];
 
-  displayedColumns = ['name', 'category']; // Colunas que a tabela vai mostrar
-
-  constructor() { }
+  displayedColumns = ['name', 'category'];
 }
